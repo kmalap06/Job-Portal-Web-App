7cfef94b43f2274cd14c7cf26ffdd452386ff59f
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2023-04-25 01:00:12 +0530

    Applied Some Minor Changes

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -4,9 +4,9 @@ const dbConnect = async () => {
     try {
         const MONGO_URL = process.env.MONGO_URL;
         const dbconn = await mongoose.connect(MONGO_URL);
-        console.log(`MONGODB Connected: ${dbconn.connection.host}`);
+        console.log(`MONGODB Connected: ${dbconn.connection.host}`.green);
     } catch (error) {
-        console.log(error.message);
+        console.log(error.message.red);
     }
 };
 
