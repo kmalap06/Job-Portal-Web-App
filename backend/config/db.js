f568c89213731caff17735e19876ebbb1e2dadae
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2023-04-26 19:23:01 +0530

    Minor Change

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const dbConnect = async () => {
     try {
         const MONGO_URL = process.env.MONGO_URL;
         const dbconn = await mongoose.connect(MONGO_URL);
-        console.log(`MONGODB Connected: ${dbconn.connection.host}`.green);
+        console.log(`MONGODB Connected: ${dbconn.connection.host}`.blue);
     } catch (error) {
         console.log(error.message.red);
     }
