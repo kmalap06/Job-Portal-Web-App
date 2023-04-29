357275d3d2186d05c3a09cbeeb5b42b488209f5e
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2023-04-29 14:14:23 +0530

    Code Cleaup Done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,41 +2,25 @@
 const { User } = require("../models/userModel");
 
 // Register Controller
-const registerController = async (req, res) => {
-    try {
-        const { firstName, lastName, email, password, location } = req.body;
+const registerController = async (req, res, next) => {
+    const { firstName, lastName, email, password, location } = req.body;
 
-        // Input Fields Verification
-        if (!firstName) return res.status(400).json("First Name Is Required!");
-        if (!lastName) return res.status(400).json("Last Name Is Required!");
-        if (!email) return res.status(400).json("Email Is Required!");
-        if (!password) return res.status(400).json("Password Is Required!");
+    // Check If User Already Exists
+    await User.findOne({ email: email });
 
-        // Check If User Already Exists
-        const userExist = await User.findOne({ email: email });
-        if (userExist)
-            return res.status(200).json({
-                success: false,
-                message: "Email ALready Exists Please Login!",
-            });
-
-        // If User Does Not Exist
-        const newUser = await User.create({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            location: location,
-        });
-        return res.status(200).json({
-            success: true,
-            message: "User Created Successfully!",
-            newUser,
-        });
-    } catch (error) {
-        console.log(error.message.red);
-        res.status(400).json({ message: error.message });
-    }
+    // If User Does Not Exist
+    const newUser = await User.create({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        location: location,
+    });
+    return res.status(200).json({
+        success: true,
+        message: "User Created Successfully!",
+        newUser,
+    });
 };
 
 const loginController = async (req, res) => {};
